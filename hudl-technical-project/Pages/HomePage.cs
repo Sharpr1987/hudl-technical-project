5e00d0d0e106baf395db9876d210c09046e9b409
Sharpr1987 <sharpr1987@gmail.com>
2022-08-10 23:05:25 -0500

    Create HomePage
    Create HomePage class to represent the homepage as an object. Add method to go to login page which returns a LoginPage object. Add method to get the current url and return it as a string. Add method to logout by clicking the logout button in the user menu. Add a method to click the login button from the homepage.

---
+++ hudl-technical-project/Pages/HomePage.cs
@@ -0,0 +1,64 @@
+﻿using hudl_technical_project.AutomationResources;
+using hudl_technical_project.BaseFiles;
+using OpenQA.Selenium;
+
+namespace hudl_technical_project.Pages
+{
+    public class HomePage : BaseApplicationPage
+    {
+        private ExplicitWait wait;
+
+        public HomePage(IWebDriver driver) : base(driver)
+        {
+            wait = new ExplicitWait(Driver);
+        }
+
+        /*
+         *Method to go to the Login page 
+         */
+
+        public LoginPage GoTo()
+        {
+            Driver.Navigate().GoToUrl("https://www.hudl.com/login");
+            return new LoginPage(Driver);
+        }
+
+        /*
+         *Method to get the current URL
+         */
+
+        public string GetCurrentURL()
+        {
+            wait.FindElement(By.XPath("//input[@title='Search']"));
+            return Driver.Url;
+        }
+
+        /*
+         * Method to logout
+         */
+
+        public void Logout()
+        {
+            userMenu.Click();
+            logoutButton.Click();
+        }
+
+        public void Login()
+        {
+            loginButton.Click();
+        }
+
+        /*
+         * Page Elements
+         */
+
+        //User Menu
+        public IWebElement userMenu => wait.FindElement(By.ClassName("hui-globalusermenu"));
+
+        //Logout Button
+        public IWebElement logoutButton => wait.FindElement(By.XPath("//a[@data-qa-id='webnav-usermenu-logout']"));
+
+        //Login Button
+        public IWebElement loginButton => wait.FindElement(By.XPath("//a[@data-qa-id='login']"));
+    }
+}
