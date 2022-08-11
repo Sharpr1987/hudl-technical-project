fbbe3b99e9075d1ef1749db95329a1492a11ed9c
Sharpr1987 <sharpr1987@gmail.com>
2022-08-10 22:55:36 -0500

    Create Project
    Create hudl-technical-project in Visual Studio as an NUnit test project.

---
+++ hudl-technical-project/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace hudl_technical_project;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
